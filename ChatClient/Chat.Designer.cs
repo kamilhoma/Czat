72cc824cc580baeaff937a09fcd68ad4347a4bd5
kamilhoma <kamil.homa@gmail.com>
2016-01-20 23:01:42 +0100

    prawie

--- ChatClient/Chat.Designer.cs
+++ ChatClient/Chat.Designer.cs
@@ -28,18 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.grpUserList = new System.Windows.Forms.GroupBox();
             this.usersOnline = new System.Windows.Forms.ListBox();
             this.grpWiadomosci = new System.Windows.Forms.GroupBox();
+            this.messagesList = new System.Windows.Forms.ListBox();
             this.btnWyslij = new System.Windows.Forms.Button();
             this.textBoxWiadomosc = new System.Windows.Forms.TextBox();
-            this.rtbOknoRozmowy = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.grpUserList.SuspendLayout();
             this.grpWiadomosci.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpUserList
             // 
+            this.grpUserList.Controls.Add(this.button1);
             this.grpUserList.Controls.Add(this.usersOnline);
             this.grpUserList.Location = new System.Drawing.Point(571, 12);
             this.grpUserList.Name = "grpUserList";
@@ -53,14 +59,14 @@
             this.usersOnline.FormattingEnabled = true;
             this.usersOnline.Location = new System.Drawing.Point(16, 19);
             this.usersOnline.Name = "usersOnline";
-            this.usersOnline.Size = new System.Drawing.Size(127, 264);
+            this.usersOnline.Size = new System.Drawing.Size(127, 238);
             this.usersOnline.TabIndex = 0;
             // 
             // grpWiadomosci
             // 
+            this.grpWiadomosci.Controls.Add(this.messagesList);
             this.grpWiadomosci.Controls.Add(this.btnWyslij);
             this.grpWiadomosci.Controls.Add(this.textBoxWiadomosc);
-            this.grpWiadomosci.Controls.Add(this.rtbOknoRozmowy);
             this.grpWiadomosci.Location = new System.Drawing.Point(12, 12);
             this.grpWiadomosci.Name = "grpWiadomosci";
             this.grpWiadomosci.Size = new System.Drawing.Size(516, 296);
@@ -68,11 +74,19 @@
             this.grpWiadomosci.TabStop = false;
             this.grpWiadomosci.Text = "Wiadomości";
             // 
+            // messagesList
+            // 
+            this.messagesList.FormattingEnabled = true;
+            this.messagesList.Location = new System.Drawing.Point(9, 27);
+            this.messagesList.Name = "messagesList";
+            this.messagesList.Size = new System.Drawing.Size(501, 225);
+            this.messagesList.TabIndex = 3;
+            // 
             // btnWyslij
             // 
-            this.btnWyslij.Location = new System.Drawing.Point(435, 235);
+            this.btnWyslij.Location = new System.Drawing.Point(437, 265);
             this.btnWyslij.Name = "btnWyslij";
-            this.btnWyslij.Size = new System.Drawing.Size(75, 55);
+            this.btnWyslij.Size = new System.Drawing.Size(75, 22);
             this.btnWyslij.TabIndex = 2;
             this.btnWyslij.Text = "&Wyślij";
             this.btnWyslij.UseVisualStyleBackColor = true;
@@ -80,26 +94,48 @@
             // 
             // textBoxWiadomosc
             // 
-            this.textBoxWiadomosc.Location = new System.Drawing.Point(9, 235);
-            this.textBoxWiadomosc.Multiline = true;
+            this.textBoxWiadomosc.Location = new System.Drawing.Point(9, 266);
             this.textBoxWiadomosc.Name = "textBoxWiadomosc";
-            this.textBoxWiadomosc.Size = new System.Drawing.Size(408, 55);
+            this.textBoxWiadomosc.Size = new System.Drawing.Size(422, 20);
             this.textBoxWiadomosc.TabIndex = 1;
+            this.textBoxWiadomosc.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxWiadomosc_KeyPress);
             // 
-            // rtbOknoRozmowy
+            // timer1
             // 
-            this.rtbOknoRozmowy.Location = new System.Drawing.Point(9, 33);
-            this.rtbOknoRozmowy.Name = "rtbOknoRozmowy";
-            this.rtbOknoRozmowy.ReadOnly = true;
-            this.rtbOknoRozmowy.Size = new System.Drawing.Size(501, 190);
-            this.rtbOknoRozmowy.TabIndex = 0;
-            this.rtbOknoRozmowy.Text = "";
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(530, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Wyloguj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 600000;
             // 
             // Chat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(740, 312);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.grpUserList);
             this.Controls.Add(this.grpWiadomosci);
             this.Name = "Chat";
@@ -108,6 +144,7 @@
             this.grpWiadomosci.ResumeLayout(false);
             this.grpWiadomosci.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -118,6 +155,10 @@
         private System.Windows.Forms.GroupBox grpWiadomosci;
         private System.Windows.Forms.Button btnWyslij;
         private System.Windows.Forms.TextBox textBoxWiadomosc;
-        private System.Windows.Forms.RichTextBox rtbOknoRozmowy;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox messagesList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
--- ChatClient/Chat.cs
+++ ChatClient/Chat.cs
@@ -16,44 +16,161 @@ namespace ChatClient
         public Chat(string userName)
         {
             InitializeComponent();
-            
-            getOnlineUsers();
+            label1.Text = userName;
+            updateChat();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            updateChat();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string q1 = "select data from Messages join Users on Messages.user_id = Users.Id where login = '" + label1.Text + "';";
+                SqlCommand com1 = new SqlCommand(q1, connection);
+
+                connection.Open();
+                using (SqlDataReader reader = com1.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var data = reader["data"];
+                        if (DateTime.Now - (DateTime)data > TimeSpan.FromMinutes(10))
+                        {
+                            logout();
+                            MessageBox.Show("Zostałeś wylogowany z powodu nieaktywnosci.");
+                            this.Close();
+                        }
+                    }
+                }
+                connection.Close();
+            }
         }
 
         string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\projekty\Repos\Czat\ChatServer\Database.mdf;Integrated Security=True";
 
-        private void getOnlineUsers()
+        private void btnWyslij_Click(object sender, EventArgs e)
+        {
+            sendMsg();
+        }
+
+        private void sendMsg()
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
+                string q1 = "insert into Messages(user_id, tresc, data) VALUES((select Id from Users where login = '" + label1.Text + "'),'" + textBoxWiadomosc.Text + "',CAST('" + DateTime.Now.ToString("yyyy-MM-dd HH:MM:ss") + "' as datetime2));";
+                string q2 = "select top 25 * from Messages";
+                SqlCommand com1 = new SqlCommand(q1, connection);
+                SqlCommand com2 = new SqlCommand(q2, connection);
+
                 connection.Open();
-                string q1 = "select login from users where online = 1";
+                com1.ExecuteNonQuery();
+                com2.ExecuteNonQuery();
+                connection.Close();
+                textBoxWiadomosc.Text = "";
+                updateMessages();
+            }
+        }
+
+        private void getOnlineUsers()
+        {
+            usersOnline.Items.Clear();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string q1 = "select login, kolor from users where online = 1";
                 SqlCommand com1 = new SqlCommand(q1, connection);
 
                 using (SqlDataReader reader = com1.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        var myString = reader["login"].ToString(); ;
+                        var myString = reader["login"].ToString();
+                        var color = reader["kolor"].ToString();
                         usersOnline.Items.Add(myString);
+
                     }
                 }
-
                 connection.Close();
             }
         }
 
-        private void btnWyslij_Click(object sender, EventArgs e)
+        private void updateMessages()
+        {
+            messagesList.Items.Clear();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string q1 = "select data, login, tresc from( select top 25 Messages.Id, data, login, tresc from Messages join Users on Messages.user_id = Users.Id ORDER BY Messages.data desc) as a order by a.Id asc";
+                SqlCommand com1 = new SqlCommand(q1, connection);
+                connection.Open();
+                using (SqlDataReader reader = com1.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        DateTime data = (DateTime)reader["data"];
+                        var myString = data.ToString("dd-MM-yyyy HH:MM:ss") + "   " + reader["login"].ToString() + " :  " + reader["tresc"].ToString();
+                        List<string> wiadomosci = new List<string>();
+                        wiadomosci.Add(myString);
+                        wiadomosci.Reverse();
+                        foreach (var item in wiadomosci)
+                        {
+                            messagesList.Items.Add(item);
+                        }
+                    }
+                    
+                }
+                connection.Close();
+            }
+        }
+
+        private void updateChat()
+        {
+            updateMessages();
+            getOnlineUsers();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            logout();
+            this.Close();
+        }
+
+        private void logout()
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                //string q1 = "insert into Messages(user id, tresc, data) VALUES('" + userName + "','" + textBoxWiadomosc.Text + "','" + DateTime.Now.ToString("ddMMyyyy HH:MM:SS");
-                string q2 = "select top 25 * from Messages";
-                
+                string q1 = "update Users set online = 0 where login = '" + label1.Text + "';";
+                string q2 = "INSERT into Logs(user_id,data,zdarzenie) VALUES ((select Id from Users where login = '" + label1.Text + "'), CAST('" + DateTime.Now.ToString("yyyy-MM-dd HH:MM:ss") + "' as datetime2), 'logout');";
+                SqlCommand com1 = new SqlCommand(q1, connection);
+                SqlCommand com2 = new SqlCommand(q2, connection);
+
                 connection.Open();
-                // Do stuff here
+                com1.ExecuteNonQuery();
+                com2.ExecuteNonQuery();
                 connection.Close();
             }
+        }
+
+        private void textBoxWiadomosc_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar) == 13)
+            {
+                sendMsg();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            logout();
         }
     }
 }
